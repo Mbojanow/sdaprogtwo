8de43a92b9a8d089a769bf29194d5a394f68eb8c
Michal Bojanowski <bojanowski.michal89@gmail.com>
2019-08-18 20:21:03 +0200

    Prog2: strD

--- src/main/java/pl/sdacademy/prog/strA/AccountDataStatisticsService.java
+++ src/main/java/pl/sdacademy/prog/strA/AccountDataStatisticsService.java
@@ -2,11 +2,14 @@ package pl.sdacademy.prog.strA;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AccountDataStatisticsService {
+
+    private static final int MIN_COUNTRY_LEN = 3;
+    private static final int MIN_CURRENCY_LEN = 3;
+    private static final int CURRENCY_SHORT_NAME = 3;
 
     public AccountDataStatisticsService() {
     }
@@ -48,5 +51,31 @@ public class AccountDataStatisticsService {
                 .collect(Collectors.groupingBy(AccountData::getCurrencyShortName))
                 .entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().size()));
+    }
+
+    public Double sumAmmounts(final List<AccountData> accountsData) {
+        return accountsData.stream()
+                .mapToDouble(AccountData::getAmount).sum();
+    }
+
+    public boolean countryWithNameLongerThan(final int charNum, final List<AccountData> accountsData) {
+        return accountsData.stream()
+                .map(AccountData::getCountry)
+                .anyMatch(country -> country.length() >= charNum);
+    }
+
+    public boolean isAllDataCorrect(final List<AccountData> accountsData) {
+        return accountsData.stream()
+                .anyMatch(this::isDataCorrect);
+    }
+
+    private boolean isDataCorrect(final AccountData accountData) {
+        final String currencyShortName = accountData.getCurrencyShortName();
+        return accountData.getCountry().length() >= MIN_COUNTRY_LEN &&
+                accountData.getCurrencyFullName().length() >= MIN_CURRENCY_LEN &&
+                accountData.getCurrencyShortName().length() == CURRENCY_SHORT_NAME &&
+                currencyShortName.equals(currencyShortName.toLowerCase());
+
+
     }
 }
