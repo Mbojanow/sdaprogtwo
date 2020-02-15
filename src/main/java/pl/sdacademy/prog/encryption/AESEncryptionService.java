25792c629cf9dad02c1ff7c57b4c5ea427ef3fbc
Michal Bojanowski <bojanowski.michal89@gmail.com>
2020-02-15 18:03:31 +0100

    aes fix

--- src/main/java/pl/sdacademy/prog/encryption/AESEncryptionService.java
+++ src/main/java/pl/sdacademy/prog/encryption/AESEncryptionService.java
@@ -1,29 +1,22 @@
 package pl.sdacademy.prog.encryption;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Random;
-import java.util.stream.Collectors;
+import pl.sdacademy.prog.streams.SdaException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-
-import pl.sdacademy.prog.streams.SdaException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class AESEncryptionService {
 
-//  • ścieżka pliku źródłowego
-//• ścieżka pliku wyjściowego
-//• tryb - 1 - enkrypcja, 2 - dekrypcja
-//• seed do generowania losowej liczby
 
   public void executeAESOperation(final String sourcePath, final String outputPath,
                                   final int operationMode, final int keyGenerationSeed) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
@@ -35,17 +28,17 @@ public class AESEncryptionService {
 
     // możemy wykorzystać TRY-WITH-RESOURCES bo BufferedReader implementuj
     // Closeable który rozszerza AutoCloseable
-    final String fileInput;
-    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(sourcePath))) {
-      fileInput = bufferedReader.lines().collect(Collectors.joining());
+    final byte[] fileInput;
+    try (FileInputStream fileInputStream = new FileInputStream(new File(sourcePath))) {
+      fileInput = fileInputStream.readAllBytes();
     } catch (IOException e) {
       throw new SdaException("Could not read input file");
     }
 
-    final byte[] output = cipher.doFinal(fileInput.getBytes());
+    final byte[] output = cipher.doFinal(fileInput);
 
-    try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath))) {
-      bufferedWriter.write(new String(output));
+    try (FileOutputStream fileOutputStream = new FileOutputStream(new File(outputPath))) {
+      fileOutputStream.write(output);
     } catch (IOException e) {
       throw new SdaException("Could not save output file");
     }
